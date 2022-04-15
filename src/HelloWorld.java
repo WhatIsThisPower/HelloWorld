a5138ed4d521fbc36d3ec95e8f886e284622fe8d
WhatIsThisPower <94736849+WhatIsThisPower@users.noreply.github.com>
2022-04-15 16:36:37 +0800

    增加一句中文 测试pull

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("hello!world!");
         System.out.println("hello!world!changed");
         System.out.println("hello!world!changed again");
+        System.out.println("我在GitHub网页端修改 增加了这一句代码 测试 pull");
     }
 }
